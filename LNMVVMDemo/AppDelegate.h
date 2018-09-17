51cf32814486f364ba680b90cd8aee51d65a62d3
laiguohua <469790503@qq.com>
2018-09-17 15:54:10 +0800

    Initial Commit

---
+++ LNMVVMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LNMVVMDemo
+//
+//  Created by Mr.lai on 2018/9/17.
+//  Copyright © 2018年 Mr.lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LNMVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LNMVVMDemo
+//
+//  Created by Mr.lai on 2018/9/17.
+//  Copyright © 2018年 Mr.lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
